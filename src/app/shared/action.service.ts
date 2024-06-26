4cbb60758c6d4a3e05096e8a01c3fa87886d29fa
VIS-WAS <vishwas.ms8830@gmail.com>
2024-06-26 21:04:14 +0530

    created standard angular project

---
+++ src/app/shared/action.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { retry } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ActionService {
+  value: boolean = false;
+  constructor() {}
+
+  changeAction() {
+    this.value = !this.value;
+
+    if (this.value) {
+      return 'Start Again';
+    } else {
+      return 'Done';
+    }
+  }
+}
---
+++ src/app/shared/highlight.directive.ts
@@ -0,0 +1,23 @@
+import { Directive, ElementRef, Renderer2 } from '@angular/core';
+
+@Directive({
+  selector: '[appHighlight]',
+})
+export class HighlightDirective {
+  constructor(private element: ElementRef, private renderer: Renderer2) {}
+
+  ngOnInit() {
+    this.renderer.setStyle(
+      this.element.nativeElement,
+      'backgroundColor',
+      '#FEF5E7'
+    );
+    this.renderer.setStyle(
+      this.element.nativeElement,
+      'border',
+      '#F5B041 1.5px solid'
+    );
+    this.renderer.setStyle(this.element.nativeElement, 'borderRadius', '5px');
+    this.renderer.setStyle(this.element.nativeElement, 'padding', '1.5rem');
+  }
+}
