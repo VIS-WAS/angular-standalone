988e4d51cb9041da16c811b6438146335eee77a8
VIS-WAS <vishwas.ms8830@gmail.com>
2024-06-27 20:01:01 +0530

    deleted module class as there is no need of @ngModule()

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { HomeComponent } from './home/home.component';
 
 @Component({
+  standalone: true,
+  imports: [HomeComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'angular-standalone';
